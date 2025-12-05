17ed14523f932c3048bfd767a4955d198efcd19a
ZV <wazolee@gmail.com>
2025-12-05 22:29:52 +0100

    end course

--- code/main.go
+++ code/main.go
@@ -8,11 +8,11 @@ import (
 	// "time"
 )
 
-
 type score struct {
 	name  string
 	score int
 }
+
 func main() {
 
 	
@@ -56,31 +56,43 @@ func main() {
 
 func loopDemo(scores []score) {
 	shouldContinue := true
-	fmt.Println("Menu:")
-	fmt.Println("1 - Add score")
-	fmt.Println("2 - List scores")
-	fmt.Println("q - Quit")
+	
 	for shouldContinue {
+		printMenu()
 		var option string = "0"
 		fmt.Scanln(&option)
 		switch option{
-		case "1":
-			
-			fmt.Println("Enter a name and score: ")
-			var name, rawScore string
-			fmt.Scanln(&name, &rawScore)
-			s, _ := strconv.Atoi(rawScore)
-			scores = append(scores, score{name: name, score: s})
-			fmt.Printf("Name: %s, Score: %d\n", name, s)		
+		case "1":			
+			scores = append(scores, addStudent())
 		case "2":
-			fmt.Println("Student scores")
-			fmt.Println(strings.Repeat("-", 14))
-			for _, sc := range scores {
-				fmt.Println(sc.name, ":", sc.score)
-			}
+			printReport(scores)
 		case "q":
 			shouldContinue = false
 		}
 
 	}
+}
+
+func addStudent() score {
+	fmt.Println("Enter a name and score: ")
+	var name, rawScore string
+	fmt.Scanln(&name, &rawScore)
+	s, _ := strconv.Atoi(rawScore)	
+	fmt.Printf("Name: %s, Score: %d\n", name, s)
+	return score{name: name, score: s}
+}
+
+func printReport(scores []score) {
+	fmt.Println("Student scores")
+	fmt.Println(strings.Repeat("-", 14))
+	for _, sc := range scores {
+		fmt.Println(sc.name, ":", sc.score)
+	}
+}
+
+func printMenu(){
+	fmt.Println("Menu:")
+	fmt.Println("1 - Add score")
+	fmt.Println("2 - List scores")
+	fmt.Println("q - Quit")
 }
